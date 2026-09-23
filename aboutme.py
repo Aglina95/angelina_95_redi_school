abd68c43c0a0dc8831a3eec7cd8d919b076e3bf6
Angelina Demirtzoglou <77700667+Aglina95@users.noreply.github.com>
2026-09-23 13:52:12 +0200

    aboutme

---
+++ aboutme.py
@@ -0,0 +1,19 @@
+name = "Angelina"
+age = 31
+city = "Copenhagen"
+language = ["Greek", "English"]
+is_student = "Intro to Programming"
+
+#print variables
+print(name)
+print(age)
+print(city)
+print(language)
+print(is_student)
+
+#check types with type()
+print(type(name))
+print(type(age))
+print(type(city))
+print(type(language))
+print(type(is_student))
--- example.py
+++ example.py
@@ -1,1 +1,20 @@
-print("hi, Alice")
+name = input("What's your name? ")
+
+course = input("Which course are you enrolled in? ")
+
+if course.casefold() != "Intro to programming".casefold():
+    print("Hi," , name.title(), "You are in the wrong classroom!!! This is ", course.title())
+
+else:
+    print("Hi, " , name.title(), "you're in the right classroom! Welcome to ", course.title())
+
+
+                
+
+
+#age = input("How old are you? ")
+#print("My name is ", name)
+#print("I am", age , "years old")
+#print("You're in the right classroom")
+
+
